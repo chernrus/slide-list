4f0d90b5d3f2084ec80c9662787b883d9a0c428a
Ruslan Chernov <chernruslan@gmail.com>
2017-03-10 16:02:29 +0700

    refs #24 Добавлены стили и анимация раскрывающегося списка, можно выбрать режим раскрытия, адаптация

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,33 @@
 
   var headers = document.getElementsByClassName('header-list');
   var contents = document.getElementsByClassName('content');
+  var collapseSet = document.getElementById('collapse');
   var arr = [];
   var j;
   arr.push.apply(arr,headers);
-  console.log(headers);
 
+  //Закрытие остальных элементов - коллизия
   var collapse = function (index) {
     for(j = 0; j < contents.length; j++) {
-      if(contents[j].style.display == 'block' &&  j!=index) {
-        contents[j].style.display = 'none';
+      if(contents[j].style.maxHeight &&  j!=index) {
+        contents[j].style.maxHeight = null;
       }
     }
   };
 
+  //действия на клик на один из элементов
   arr.forEach(function(item, i, arr) {
     item.onclick = function(event) {
       //установить display none
-      if(contents[i].style.display == 'block') {
-        contents[i].style.display = 'none';
+      if(contents[i].style.maxHeight) {
+        contents[i].style.maxHeight = null;
       } else {
-        contents[i].style.display = 'block';
+        contents[i].style.maxHeight = contents[i].scrollHeight + "px"; //Анимация от 0 до maxHeight
       }
 
-      collapse(i);
+      if(collapseSet.checked) {
+        collapse(i);
+      }
     };
   });
 })(this);
